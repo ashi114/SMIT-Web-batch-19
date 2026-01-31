ca878550ff075c94335e441fd510cad59f4320e2
ashi114 <arshadmansoor02@gmail.com>
2026-01-31 15:20:28 +0500

    JS Loops

--- js/app.js
+++ js/app.js
@@ -57,8 +57,100 @@
 
 // using Turnary operator
 
-let age = 15
+// let age = 15
 
-let result = age >= 18 ? "adult" : "not adult"
+// let result = age >= 18 ? "adult" : "not adult"
 
-console.log(result)
+// console.log(result)
+
+// let myArray = [1,2,3]
+
+// let myNumber = Number(prompt("Please enter your number"))
+
+// myArray.push(myNumber)
+
+// alert(myArray)
+
+// If / else practice
+
+// let age = 50;
+// let gender = "Female"
+
+// if (age < 18 && gender == "Female") {
+//     console.log("Under Aged and womens are not allowed")
+// } else if (age >= 45 && gender == "Female") {
+//     console.log("Over aged and womens not allowed")
+// } else {
+//     console.log("Approved")
+// }
+
+// Loops
+
+// print of Loops
+
+// for (let i=0; i<=10; i++){
+//     console.log("Startig Loops")
+// }
+
+// console.log("Loop Ended")
+
+// Sum of Numbers
+
+// let sum = 0
+// let n = 10
+
+// for (let i = 0; i <= n; i++) {
+//     sum = sum + i
+
+// }
+// console.log(sum)
+
+// While Loop
+// let i = 0
+// while (i <= 5){
+//     console.log(i)
+//     i++
+// }
+
+// Do While
+// let i = 20
+// do {
+//     console.log("Do While Loop");
+//     i++;
+// }while(i<=10)
+
+// for off loop
+// let str = "Saylani"
+
+// for(let i of str){
+//     console.log("i =",i)
+// }
+
+// for in loop
+// let student = {
+//     name: "Arshad",
+//     age: 30,
+//     cgpa: 3.4,
+//     passed: true
+// }
+
+// for (let key in student){
+//     console.log("key==",key,"value==",student[key])
+// }
+
+// Print all Numbers from 0 to 100
+// for(let i =0;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// let number = 25;
+// let input = Number(prompt("please enter your number"))
+
+// while(number !== input){
+//     let input = Number(prompt("please enter the matching number"))
+//     Break
+// }
+
+// console.log("Correct Number selected")
