ce0906bfa19ab3df7781a31dd27a8d96b95bbbfe
ashi114 <arshadmansoor02@gmail.com>
2026-03-28 09:10:57 +0500

    Cricket Game Toss and option Selection Completed

--- cricket-game/app.js
+++ cricket-game/app.js
@@ -1,6 +1,7 @@
 let userName = ""
 let teamSelected = ""
 let coinSelected = ""
+let isUserBatting = null
 
 const nameWrapper = document.getElementById("name-wrapper");
 const nameNextButton = nameWrapper.querySelector("button");
@@ -11,6 +12,9 @@ const teams = document.getElementsByClassName("team")
 
 const tossWrapper = document.getElementById("toss-wrapper")
 const coins = tossWrapper.getElementsByClassName("coin")
+
+const battingWrapper = document.getElementById("batting-selection-wrapper")
+const battingOptions = battingWrapper.getElementsByClassName("batting-option")
 
 function getUsername() {
     const value = nameInput.value
@@ -52,8 +56,10 @@ function selectedCoin(event) {
         const isUserWon = winningSide === selectedSide;
         if (isUserWon) {
             alert("Congrats you have won the toss")
+            proceedToNextPageAfterCoinSelection()
         } else {
             alert("You fail to win the toss")
+            autoBattingBowlingSelection()
         }
     }
 }
@@ -63,4 +69,56 @@ function generateCoinResult() {
     const random = Math.random();
     const side = Math.ceil(random * 2)
     return side.toString();
+}
+
+function generateCoinResult() {
+    const random = Math.random();
+    const side = Math.ceil(random * 2)
+    return side.toString();
+}
+
+function proceedToNextPageAfterCoinSelection() {
+    tossWrapper.remove()
+    battingWrapper.classList.replace("none", "flex")
+};
+
+for (let i = 0; i < battingOptions.length; i++) {
+    const battingOption = battingOptions[i]
+    battingOption.addEventListener("click", selectedOption)
+}
+
+function selectedOption(event) {
+    userOptionSelected = event.target.innerText;
+    userOptionId = event.target.id;
+    const isConfirm = confirm(`Are you sure you want to ${userOptionSelected}`);
+
+    if (!isConfirm) return;
+
+    if (userOptionId === "Batting") {
+        isUserBatting = true;
+    } else {
+        isUserBatting = false
+    }
+
+    battingWrapper.remove()
+    showAndUpdatePlay();
+}
+
+
+function autoBattingBowlingSelection() {
+    const randomNumber = Math.random();
+    const selectedSide = Math.ceil(randomNumber * 2);
+
+    if (selectedSide === 1) {
+        isUserBatting = false
+    } else {
+        isUserBatting = True
+    }
+
+    tossWrapper.remove();
+    showAndUpdatePlay();
+}
+
+function showAndUpdatePlay(){
+    
 }
